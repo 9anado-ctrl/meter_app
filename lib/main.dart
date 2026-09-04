3dcbf7436b8244edb99a10a8f72036643548d7e2
9anado-ctrl <9anado@gmail.com>
2026-09-05 01:42:20 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+name: meter_app
+description: "تطبيق الفواتير والعدادات"
+publish_to: 'none'
+version: 1.0.0+1
+
+environment:
+  sdk: '>=3.0.0 <4.0.0'
+
+dependencies:
+  flutter:
+    sdk: flutter
+  cupertino_icons: ^1.0.8
+  url_launcher: ^6.3.0
+
+flutter:
+  uses-material-design: true
